999515779e4c82401d33b3ce92160a9c291c385a
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2018-09-11 13:07:53 -0700

    v1

--- pages/projects.js
+++ pages/projects.js
@@ -3,7 +3,8 @@ import css from "../styles.scss"
 
 export default () => (
     <Layout>
-      <p className={css.hello}>Beau has worked on many projects</p>
-      <p className={css.desc}>His favorites include the mountain projects and the coding projects</p>
+      <div className={css.projectBox}>
+        Hello World
+      </div>
     </Layout>
 )
