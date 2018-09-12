2de9631fe3b3b1eb78c696847daa8991dd3b0f57
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2018-09-11 18:56:48 -0700

    v1 education page

--- pages/education.js
+++ pages/education.js
@@ -3,7 +3,26 @@ import css from "../styles.scss"
 
 export default () => (
     <Layout>
-      <p className={css.hello}>Beau went to school</p>
-      <p className={css.desc}>Mountain school and coding school made him who he is</p>
+      <div className={css.educationBox}>
+        <h3>Harvey Mudd College | Bachelor | 2021</h3>
+        <p className={css.subtext}><em>Computer Science, Economics</em></p>
+        <p className={css.description}>Courses (In Progress):</p>
+        <ul className={css.courseList}>
+          <li>Data Structures and Program Development</li>
+          <li>Computer Science Practicum</li>
+          <li>Discrete Math</li>
+          <li>Multivariable Calculus</li>
+          <li>Financial Economics</li>
+        </ul>
+        <p className={css.description}>Completed:</p>
+        <ul className={css.courseList}>
+          <li>Principles & Practice: Computer Science</li>
+          <li>Engineering of Autonomous Vehicles</li>
+          <li>Linear Algebra</li>
+          <li>Differential Equations</li>
+          <li>Proof-Based Calculus</li>
+          <li>Probability and Statistics</li>
+        </ul>
+      </div>
     </Layout>
 )
