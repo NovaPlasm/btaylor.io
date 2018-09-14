3f2efc4c97382cff2b502ae7b68699e594d57faf
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2018-09-13 21:31:34 -0700

    Completed

--- pages/experience.js
+++ pages/experience.js
@@ -3,7 +3,21 @@ import css from "../styles.scss"
 
 export default () => (
     <Layout>
-      <p className={css.hello}>Beau has experience doing things</p>
-      <p className={css.desc}>Such as climbing mountains and writing code that works</p>
+      <div className="experienceBox">
+        <h3>Developer Intern | <a href="www.cbtnuggets.com">CBT Nuggets</a> | May 2018 - Present</h3>
+        <p className="description">Full-stack developer responsible for implementing a new content platform for CBT Nuggets.  Daily responsibilities included web development using ReactJS, email template optimization for all modern and highly used clients, and development of web landers used at the Society for Human Resource Management Conference</p>
+        <br />
+        <br />
+        <h3>Computer Information Services Intern | <a href="https://www.hmc.edu/cis/">Harvey Mudd College</a> | Sept 2017-Present</h3>
+        <p className="description">Responsible for creating the asset management program used by Harvey Mudd College, as well as streamlining the printer reporting functionality used by the college</p>
+        <br />
+        <br />
+        <h3>Research Intern | University of Oregon | 2015</h3>
+        <p className="description">Worked with Dr. Michael Kinsey, Ph.D. from MIT in Electrical Engineering and Computer Science, using miniature robots in order to simulate the spread of diseases.</p>
+        <br />
+        <br />
+        <h3>Freelance Programmer | UPS | 2015</h3>
+        <p className="description">Developed a program for a local UPS store that documents in-store computer time usage and generates a corresponding invoice</p>
+      </div>
     </Layout>
 )
