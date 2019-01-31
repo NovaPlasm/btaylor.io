a96ba68e42ff59ac93269a49fe34c176548ace24
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2019-01-31 13:03:46 -0800

    cleaning up

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout.js'
-import css from "../styles.scss"
-
-export default () => (
-    <Layout>
-      <p className="hello">Hi, I'm Beau</p>
-      <p className="desc">I climb mountains and write code</p>
-    </Layout>
-)
