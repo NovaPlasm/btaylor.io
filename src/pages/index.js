b5bd0d8e1d32b279beafe7d40cf0da61a67e78ff
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2019-01-31 12:32:18 -0800

    directory change

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout.js'
+import css from "../styles.scss"
+
+export default () => (
+    <Layout>
+      <p className="hello">Hi, I'm Beau</p>
+      <p className="desc">I climb mountains and write code</p>
+    </Layout>
+)
