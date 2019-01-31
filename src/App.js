4b74cceb993eb74911fe57d5aa9d5e7aa7fd6ee6
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2019-01-31 13:54:27 -0800

    migration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import { Education, Experience, Projects, Skills, Contact, Home } from './pages';
+import { Education, Experience, Projects, Skills, Contact, Home } from './pages/';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <link rel="stylesheet" href="/_next/static/style.css" />
-        
+
         <Route exact path="/" component={Home} />
         <Route path="/education" component={Education} />
         <Route path="/experience" component={Experience} />
