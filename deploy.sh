bc8b5615138993b832e1d462094f79ae2d24ee4d
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2019-02-14 20:44:46 -0800

    Changed color

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 CYAN='\033[0;36m'
-PURPLE='\033[0;35m'
-printf "${CYAN}Deploying current build to production!${PURPLE}\n"
+GREEN='\033[0;32m'
+printf "${CYAN}Deploying current build to production!${GREEN}\n"
 git push prod master
